9aee5ff6e4ee5b6bf5c53fb7abd7245cd2c9174c
Gergely David <david.gergely.eu@gmail.com>
2020-05-15 10:10:38 +0200

    ExceptionTest is checking int age

--- Progtech/src/Testing/AgeAttributeModifiersTest.java
+++ Progtech/src/Testing/AgeAttributeModifiersTest.java
@@ -1,7 +1,11 @@
 package Testing;
 
 import Attributes.AgeAttributeModifiers;
+import Creatures.Castes.Bard;
+import Creatures.Castes.Caste;
 import Creatures.Castes.Race;
+import Exceptions.InvalidAgeException;
+import Exceptions.InvalidAttributeException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,6 +19,13 @@ public class AgeAttributeModifiersTest {
         assertEquals(-5, test.StrengthAgeModifier(Race.HUMAN, 80));
         assertEquals(0, test.DexterityAgeModifier(Race.DWARF, 51));
         assertEquals(-2, test.StrengthAgeModifier(Race.ORC, 12));
+    }
+
+    @Test(
+            expected = InvalidAgeException.class
+    )
+    public void invalidAgeTest() throws InvalidAgeException, InvalidAttributeException {
+        Bard bard = new Bard("Tier", 8, Race.HUMAN, Caste.BARD, 1);
     }
 
 }
